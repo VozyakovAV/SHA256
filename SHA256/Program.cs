c40fa5180f6a5145d49442ed07b64f7d5b1799e8
andrew <andvoz@mail.ru>
2018-06-06 17:36:16 +0400

    added

---
+++ SHA256/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Не заданы аргументы командной строки");
+                return;
+            }
+
+            Console.WriteLine(GetHash(args[0], args[1]));
+        }
+
+        public static String GetHash(String text, String key)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] textBytes = encoding.GetBytes(text);
+            Byte[] keyBytes = encoding.GetBytes(key);
+
+            Byte[] hashBytes;
+
+            using (HMACSHA256 hash = new HMACSHA256(keyBytes))
+                hashBytes = hash.ComputeHash(textBytes);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
